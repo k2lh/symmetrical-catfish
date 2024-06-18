1e9184db98917033e6ca7e22cf882132b734a4bd
kusuriuri <hemengting@gmail.com>
2024-06-18 18:00:55 -0400

    2nd try

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-// import store from "./store"
-// import vSelect from 'vue-select'
-import VueScrollTo from 'vue-scrollto'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+// import store from "./store";
+// import vSelect from 'vue-select';
+import VueScrollTo from 'vue-scrollto';
 
 import './assets/main.css'
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,109 +1,72 @@
 const path = require('path');
+const env = process.env.NODE_ENV || process.env.VUE_APP_ENV_NAME || 'production';
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {VueLoaderPlugin} = require("vue-loader");
+const { VueLoaderPlugin } = require("vue-loader");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const autoprefixer = require("autoprefixer");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: "production",
+	mode: "production",
 	devtool: "source-map",
-	entry: {
-		main: "./src/main.js",
+	// entry: {	main: "./src/main.js" },
+	entry: path.join(__dirname, 'src', 'main.js'),
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].chunk.js'
 	},
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].chunk.js',
-        // filename: "[name].[contenthash:8].js",
-        // chunkFilename: "[name].[contenthash:8].js",			
-    },
-    plugins: [
-        // new HtmlWebpackPlugin({
-            // template: 'src/index.html' // to import index.html file inside index.js
-        // }),
-	    new VueLoaderPlugin(),
-	    new CleanWebpackPlugin(),
-	    new MiniCssExtractPlugin({
-	      filename: "[name].[contenthash:8].css",
-	      chunkFilename: "[name].[contenthash:8].css",
-	    }),
-	    new htmlWebpackPlugin({
-	      template: path.resolve(__dirname, "public", "index.html"),
-	      favicon: "./public/favicon.ico",
-	    })
-    ],
-    devServer: {
-        port: 6000 // you can change the port
-    },
-    module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                use: {
-                    loader: 'vue-loader',
-                }
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                }
-            },
-	      {
-	        test: /\.(eot|ttf|woff|woff2)(\?\S*)?$/,
-	        loader: "file-loader",
-	        options: {
-	          name: "[name][contenthash:8].[ext]",
-	        }
-	      },
-	      {
-	        test: /\.(png|jpe?g|gif|webm|mp4|svg)$/,
-	        loader: "file-loader",
-	        options: {
-	          name: "[name][contenthash:8].[ext]",
-	          outputPath: "assets/img",
-	          esModule: false,
-	        }
-	      },
-	      {
-	        test: /\.s?css$/,
-	        use: [
-	          "style-loader",
-	          MiniCssExtractPlugin.loader,
-	          "css-loader",
-	          {
-	            loader: "postcss-loader",
-	            options: {
-	              plugins: () => [autoprefixer()],
-	            },
-	          },
-	          "sass-loader",
-	        ]
-	      }						
-        ]
-    },
+	optimization: {
+		splitChunks: {
+			chunks: 'all',
+		}
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: path.join(__dirname, 'dist', 'index.html'),
+			// template: path.join(__dirname, 'static', 'index.html'),
+			inject: true,
+			// template: 'src/index.html', // to import index.html file inside index.js
+			// template: path.resolve(__dirname, "public", "index.html")
+			favicon: "./public/favicon.ico"
+		}),
+		new VueLoaderPlugin(),
+    new MiniCssExtractPlugin()
+	],
+	devServer: {
+		port: 6000 // you can change the port
+	},
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader'
+			},
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				include: [path.join(__dirname, 'src')]
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					'vue-style-loader',
+					'css-loader',
+					{
+						loader: 'sass-loader',
+					}
+				]
+			},
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+		]
+	},
 	resolve: {
-	    alias: {
-	      vue$: "vue/dist/vue.runtime.esm.js",
-	    },
-	    extensions: ["*", ".js", ".vue", ".json"],
-	  },
-	  optimization: {
-	    moduleIds: "hashed",
-	    runtimeChunk: "single",
-	    splitChunks: {
-	      cacheGroups: {
-	        vendor: {
-	          test: /[\\/]node_modules[\\/]/,
-	          name: "vendors",
-	          priority: -10,
-	          chunks: "all",
-	        },
-	      },
-	    },
-	  },
-	  devServer: {
-	    historyApiFallback: true,
-	  }
-};
+		extensions: [
+			'.tsx', '.ts', '.js', '.vue'
+		]
+	},
+	devServer: {
+		historyApiFallback: true,
+	}
+}
