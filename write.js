e184f90240e52b8c5083fa6351838b2a463f2941
kusuriuri <hemengting@gmail.com>
2024-06-19 11:57:39 -0400

    le sigh

--- write.js
+++ write.js
@@ -1,17 +1,2 @@
-const fs = require('fs')
-
-// directory path
 const dir = './'
-
-// list all files in the directory
-fs.readdir(dir, (err, files) => {
-  if (err) {
-    console.log(err)
-  }
-
-  // files object contains all files names
-  // log them on console
-  files.forEach(file => {
-    console.log(file)
-  })
-})
+console.log(dir)
