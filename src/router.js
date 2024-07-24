36e3faa0f5d75aa8b86b2b078fb2fb7e40505812
kusuriuri <hemengting@gmail.com>
2024-07-24 12:50:47 -0400

    durnit

--- src/router.js
+++ src/router.js
@@ -9,7 +9,7 @@ const routes = [
   { path: '/404', name: 'NotFound', component: () => import('./views/404.vue') },
   // { path: '/:pathMatch(.*)*', name: 'NotFound', component:() => import('./views/404.vue') }
   // { path: '/:catchAll(.*)*', name: 'NotFound', component:() => import('./views/404.vue') }
-  { path: '/:catchAll(.*)*', redirect: { name: 'NotFound' }
+  { path: '/:catchAll(.*)*', redirect: { name: 'NotFound' } }
   // { path: '/:catchAll(.*)*', redirect: { name: 'NotFound', component:() => import('./views/404.vue') }
 ]
 
