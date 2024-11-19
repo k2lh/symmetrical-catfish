742ff03a4d792c5670c5a1a59d7df3b86579d177
kusuriuri <hemengting@gmail.com>
2024-11-19 14:00:22 -0500

    dmn hdr

--- src/components/globalHeader.vue
+++ src/components/globalHeader.vue
@@ -4,27 +4,27 @@
       <div class="pure-u-1-4 pure-u-md-1-8">
         <siteLogo/>
       </div>
-      <div class="pure-u-1-4 pure-u-md-1-2 boxlabel">
+      <div class="pure-u-1-4 pure-u-md-1-2 navbox">
         <router-link :to="'/'" class="exception">
           K L Hamilton
         </router-link>
       </div>
-      <div class="pure-u-1-4 pure-u-md-1-8 boxlabel">
+      <div class="pure-u-1-4 pure-u-md-1-8 navbox">
         <router-link :to="'/highlights'">
           highlights
         </router-link>
       </div>
-      <div class="pure-u-1-4 pure-u-md-1-8 boxlabel">
+      <div class="pure-u-1-4 pure-u-md-1-8 navbox">
         <router-link :to="'/archive'">
           archive
         </router-link>
       </div>
-      <!-- <div class="pure-u-1-4 pure-u-md-1-6 pure-u-lg-1-8 boxlabel">
+      <!-- <div class="pure-u-1-4 pure-u-md-1-6 pure-u-lg-1-8 navbox">
         <router-link to="/narratives">
           narratives
         </router-link>
       </div> -->
-      <div class="pure-u-1-4 pure-u-md-1-8 boxlabel">
+      <div class="pure-u-1-4 pure-u-md-1-8 navbox">
         <router-link :to="'/history'">
           history
         </router-link>
@@ -51,13 +51,16 @@
     position: fixed;
     top: 0rem;
     z-index: 100;
-    background: transparent;
+    background: #F9F9F9;
   }
 
-  .boxlabel {
+  .navbox {
+    font-weight: 700;
+    font-size: .8rem;
+    text-transform: uppercase;
+    letter-spacing: 3px;
     padding: 1rem 0 0 0;
   }
-
   .sitelogo {
     padding: 3px 0 0 2rem;
   }
