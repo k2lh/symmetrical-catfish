31bc4de10eb1dd2ce023738aaf28fc0ea10906ae
kusuriuri <hemengting@gmail.com>
2024-11-19 13:05:37 -0500

    history

--- src/views/history.vue
+++ src/views/history.vue
@@ -1,5 +1,131 @@
 <template>
   <div class="page history">
+    <div class="pure-g rows">
+      <div class="pure-u-1-1">
+        <div class="intro-text">
+          <div class="boxlabel adjusted">
+            skills
+          </div>
+        </div>
+        <div class="pure-u-1-2 pure-u-md-1-5">
+          <div class="metadata">
+            <div class="title">
+                principles
+            </div>
+          </div>
+          <ul>
+            <li>curiosity</li>
+            <li>reusable patterns</li>
+            <li>agile</li>
+            <li>kanban</li>
+            <li>leadership</li>
+            <li>written / verbal / digital communication</li>
+            <li>cross-cultural communication</li>
+            <li>critical thinking</li>
+            <li>empathy</li>
+          </ul>
+        </div>
+        <div class="pure-u-1-2 pure-u-md-1-5">
+          <div class="metadata">
+            <div class="title">
+                research
+            </div>
+          </div>
+          <ul>
+            <li>process analysis</li>
+            <li>task analysis</li>
+            <li>comparative analysis</li>
+            <li>ethnographic studies</li>
+            <li>contextual studies</li>
+            <li>agile</li>
+            <li>kanban</li>
+          </ul>
+        </div>
+        <div class="pure-u-1-2 pure-u-md-1-5">
+          <div class="metadata">
+            <div class="title">
+                design
+            </div>
+          </div>
+          <ul>
+            <li>findability</li>
+            <li>usability</li>
+            <li>interaction</li>
+            <li>low/high fidelity</li>
+            <li>wireframes</li>
+            <li>mockups</li>
+            <li>design critiques</li>
+            <li>information architecture</li>
+            <li>workflows</li>
+            <li>design principles</li>
+            <li>responsive design</li>
+            <li>adaptability</li>
+            <li>reliability</li>
+          </ul>
+        </div>
+        <div class="pure-u-1-2 pure-u-md-1-5">
+          <div class="metadata">
+            <div class="title">
+                development
+            </div>
+          </div>
+          <ul>
+            <li>coding standards</li>
+            <li>maintainable code</li>
+            <li>reusable components</li>
+            <li>code reviews</li>
+            <li>rapid prototyping</li>
+            <li>RESTful API design</li>
+            <li>application architecture</li>
+            <li>database architecture</li>
+            <li>CI/CD/CD</li>
+            <li>version control</li>
+          </ul>
+        </div>
+        <div class="pure-u-1-2 pure-u-md-1-5">
+          <div class="metadata">
+            <div class="title">
+                tools
+            </div>
+          </div>
+          <ul>
+            <li>Vue</li>
+            <li>D3.js</li>
+            <li>AngularJS</li>
+            <li>javascript</li>
+            <li>typescript</li>
+            <li>SCSS / CSS3</li>
+            <li>NPM / Yarn</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>Webpack</li>
+            <li>MongoDB</li>
+            <li>SQL</li>
+            <li>Sketch</li>
+            <li>Figma</li>
+            <li>Mural</li>
+            <li>Invision</li>
+            <li>AEM</li>
+            <li>Docker</li>
+            <li>Jenkins</li>
+            <li>Storybook</li>
+            <li>Swagger</li>
+            <li>Jira</li>
+            <li>Zenhub</li>
+            <li>Git</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="pure-g rows">
+      <div class="pure-u-1-1">
+        <div class="intro-text">
+          <div class="boxlabel adjusted">
+            experience
+          </div>
+        </div>
+      </div>
+    </div>
     <div v-for="(job, index) in positions" :key="index" class="">
       <div class="pure-g rows">
         <div class="pure-u-1-1 pure-u-md-1-4">
@@ -41,6 +167,17 @@
         </div>
       </div>
     </div>
+    <div class="pure-g rows">
+      <div class="pure-u-1-1">
+        <div class="intro-text">
+          <div class="boxlabel adjusted">
+            education
+          </div>
+          <p class="title">Bachelor of Science in Interdisciplinary Studies</p>
+          <p>George Mason University • Fairfax, VA</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -66,6 +203,28 @@
   .title {
     font-weight: 700;
     padding-bottom: .5rem;
+  }
+  .intro-text {
+    margin: 1rem;
+    padding: 2.5rem 0 1rem 1rem;
+    font-size: .95rem;
+    font-weight: 400;
+    & p {
+      line-height: 1.3;
+    }
+    & p.title {
+      padding-bottom: 0;
+    }
+  }
+  .boxlabel {
+    font-weight: 500;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+  }
+  .boxlabel.adjusted {
+    letter-spacing: 3px;
+    font-size: 1rem;
+    font-weight: 500;
   }
   .metadata {
     text-align: right;
@@ -96,9 +255,5 @@
       letter-spacing: 1px;
     }
   }
-  .boxlabel {
-    font-weight: 500;
-    text-transform: uppercase;
-    letter-spacing: 1px;
-  }
+
 </style>
