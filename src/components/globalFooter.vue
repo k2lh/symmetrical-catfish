01cd159a681c6d7fea051687b31271609ff87ae6
kusuriuri <hemengting@gmail.com>
2024-11-19 15:55:02 -0500

    footer 3

--- src/components/globalFooter.vue
+++ src/components/globalFooter.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="globalFooter">
     <div class="pure-u-1-1">
-      <p class="description">
-        <a href="https://github.com/k2lh/" target="_blank" aria-label="(opens in new window)">github repository (k2lh)</a>
-      </p>
-      <p class="description">
-        <a href="https://uk.linkedin.com/in/kl-hamilton/" target="_blank" aria-label="(opens in new window)">LinkedIn profile (klhamilton)</a>
-      </p>
+      <div class="boxed">
+        <p>no figmas were harmed in the coding of this site.</p>
+        <!-- <p class="description">
+          <a href="https://github.com/k2lh/" target="_blank" aria-label="(opens in new window)">github repository (k2lh)</a>
+        </p>
+        <p class="description">
+          <a href="https://uk.linkedin.com/in/kl-hamilton/" target="_blank" aria-label="(opens in new window)">LinkedIn profile (klhamilton)</a>
+        </p> -->
+      </div>
     </div>
   </div>
 </template>
@@ -19,6 +22,16 @@
   .globalFooter {
     border-top: 1px solid #333;
     padding: 3rem 0;
+  }
+  .boxed {
+    margin: 1rem;
+    padding: 1rem;
+    font-size: .8rem;
+    font-weight: 300;
+    border-top: 1px solid #c6c5c5;
+    & p {
+      line-height: 1.2;
+    }
   }
   .inset {
     margin: 0 2rem;
