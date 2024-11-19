4a59701620cd03b7c9fddcd5126d652d45fbc729
kusuriuri <hemengting@gmail.com>
2024-11-19 13:47:47 -0500

    history 2

--- src/views/history.vue
+++ src/views/history.vue
@@ -2,120 +2,130 @@
   <div class="page history">
     <div class="pure-g rows">
       <div class="pure-u-1-1">
-        <div class="intro-text">
+        <div class="section-title">
           <div class="boxlabel adjusted">
             skills
           </div>
         </div>
-        <div class="pure-u-1-2 pure-u-md-1-5">
-          <div class="metadata">
-            <div class="title">
-                principles
-            </div>
-          </div>
-          <ul>
-            <li>curiosity</li>
-            <li>reusable patterns</li>
-            <li>agile</li>
-            <li>kanban</li>
-            <li>leadership</li>
-            <li>written / verbal / digital communication</li>
-            <li>cross-cultural communication</li>
-            <li>critical thinking</li>
-            <li>empathy</li>
-          </ul>
-        </div>
-        <div class="pure-u-1-2 pure-u-md-1-5">
-          <div class="metadata">
-            <div class="title">
-                research
-            </div>
-          </div>
-          <ul>
-            <li>process analysis</li>
-            <li>task analysis</li>
-            <li>comparative analysis</li>
-            <li>ethnographic studies</li>
-            <li>contextual studies</li>
-            <li>agile</li>
-            <li>kanban</li>
-          </ul>
-        </div>
-        <div class="pure-u-1-2 pure-u-md-1-5">
-          <div class="metadata">
-            <div class="title">
-                design
-            </div>
-          </div>
-          <ul>
-            <li>findability</li>
-            <li>usability</li>
-            <li>interaction</li>
-            <li>low/high fidelity</li>
-            <li>wireframes</li>
-            <li>mockups</li>
-            <li>design critiques</li>
-            <li>information architecture</li>
-            <li>workflows</li>
-            <li>design principles</li>
-            <li>responsive design</li>
-            <li>adaptability</li>
-            <li>reliability</li>
-          </ul>
-        </div>
-        <div class="pure-u-1-2 pure-u-md-1-5">
-          <div class="metadata">
-            <div class="title">
-                development
-            </div>
-          </div>
-          <ul>
-            <li>coding standards</li>
-            <li>maintainable code</li>
-            <li>reusable components</li>
-            <li>code reviews</li>
-            <li>rapid prototyping</li>
-            <li>RESTful API design</li>
-            <li>application architecture</li>
-            <li>database architecture</li>
-            <li>CI/CD/CD</li>
-            <li>version control</li>
-          </ul>
-        </div>
-        <div class="pure-u-1-2 pure-u-md-1-5">
-          <div class="metadata">
-            <div class="title">
-                tools
-            </div>
-          </div>
-          <ul>
-            <li>Vue</li>
-            <li>D3.js</li>
-            <li>AngularJS</li>
-            <li>javascript</li>
-            <li>typescript</li>
-            <li>SCSS / CSS3</li>
-            <li>NPM / Yarn</li>
-            <li>Node</li>
-            <li>Express</li>
-            <li>Webpack</li>
-            <li>MongoDB</li>
-            <li>SQL</li>
-            <li>Sketch</li>
-            <li>Figma</li>
-            <li>Mural</li>
-            <li>Invision</li>
-            <li>AEM</li>
-            <li>Docker</li>
-            <li>Jenkins</li>
-            <li>Storybook</li>
-            <li>Swagger</li>
-            <li>Jira</li>
-            <li>Zenhub</li>
-            <li>Git</li>
-          </ul>
-        </div>
       </div>
+      <div class="pure-u-1-2 pure-u-md-1-6">
+        <div class="description roles">
+          <div class="boxlabel">
+            principles
+          </div>
+        </div>
+        <ul class="roles">
+          <li>curiosity</li>
+          <li>reusable patterns</li>
+          <li>agile</li>
+          <li>kanban</li>
+          <li>leadership</li>
+          <li>written / verbal / digital communication</li>
+          <li>cross-cultural communication</li>
+          <li>critical thinking</li>
+          <li>empathy</li>
+        </ul>
+      </div>
+      <div class="pure-u-1-2 pure-u-md-1-6">
+        <div class="description roles">
+          <div class="boxlabel">
+            research
+          </div>
+        </div>
+        <ul class="roles">
+          <li>process analysis</li>
+          <li>task analysis</li>
+          <li>comparative analysis</li>
+          <li>ethnographic studies</li>
+          <li>contextual studies</li>
+          <li>agile</li>
+          <li>kanban</li>
+        </ul>
+      </div>
+      <div class="pure-u-1-2 pure-u-md-1-6">
+        <div class="description roles">
+          <div class="boxlabel">
+            design
+          </div>
+        </div>
+        <ul class="roles">
+          <li>findability</li>
+          <li>usability</li>
+          <li>interaction</li>
+          <li>low/high fidelity</li>
+          <li>wireframes</li>
+          <li>mockups</li>
+          <li>design critiques</li>
+          <li>information architecture</li>
+          <li>workflows</li>
+          <li>design principles</li>
+          <li>responsive design</li>
+          <li>adaptability</li>
+          <li>reliability</li>
+        </ul>
+      </div>
+      <div class="pure-u-1-2 pure-u-md-1-6">
+        <div class="description roles">
+          <div class="boxlabel">
+            development
+          </div>
+        </div>
+        <ul class="roles">
+          <li>coding standards</li>
+          <li>maintainable code</li>
+          <li>reusable components</li>
+          <li>code reviews</li>
+          <li>rapid prototyping</li>
+          <li>RESTful API design</li>
+          <li>application architecture</li>
+          <li>database architecture</li>
+          <li>CI/CD/CD</li>
+          <li>version control</li>
+        </ul>
+      </div>
+      <div class="pure-u-1-2 pure-u-md-1-6">
+        <div class="description roles">
+          <div class="boxlabel">
+            tools
+          </div>
+        </div>
+        <ul class="roles">
+          <li>Vue</li>
+          <li>D3.js</li>
+          <li>AngularJS</li>
+          <li>javascript</li>
+          <li>typescript</li>
+          <li>SCSS / CSS3</li>
+          <li>NPM / Yarn</li>
+          <li>Node</li>
+          <li>Express</li>
+          <li>Webpack</li>
+          <li>MongoDB</li>
+          <li>SQL</li>
+        </ul>
+      </div>
+      <div class="pure-u-1-2 pure-u-md-1-6">
+        <div class="description roles">
+          <div class="boxlabel">
+            &nbsp;
+          </div>
+        </div>
+        <ul class="roles">
+          <li>Sketch</li>
+          <li>Figma</li>
+          <li>Mural</li>
+          <li>Invision</li>
+          <li>AEM</li>
+          <li>Docker</li>
+          <li>Jenkins</li>
+          <li>Storybook</li>
+          <li>Swagger</li>
+          <li>Jira</li>
+          <li>Zenhub</li>
+          <li>Git</li>
+        </ul>
+      </div>
+    </div>
     </div>
     <div class="pure-g rows">
       <div class="pure-u-1-1">
@@ -204,17 +214,17 @@
     font-weight: 700;
     padding-bottom: .5rem;
   }
-  .intro-text {
-    margin: 1rem;
-    padding: 2.5rem 0 1rem 1rem;
-    font-size: .95rem;
+  .section-title {
+    font-size: 1rem;
+    font-weight: 500;
+    letter-spacing: 3px;
+  }
+  .sub-title {
+    font-size: .85rem;
     font-weight: 400;
-    & p {
-      line-height: 1.3;
-    }
-    & p.title {
-      padding-bottom: 0;
-    }
+    margin: 0 0 0 1.5rem;
+    border: 1px solid blue;
+    text-transform: uppercase;
   }
   .boxlabel {
     font-weight: 500;
