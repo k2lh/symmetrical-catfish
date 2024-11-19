99c4a46a479b0856a54faaac113e4c135c3762e3
kusuriuri <hemengting@gmail.com>
2024-11-19 16:00:59 -0500

    footer fin

--- src/components/globalFooter.vue
+++ src/components/globalFooter.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="globalFooter">
     <div class="pure-u-1-1">
-      <div class="boxed">
-        <p>no figmas were harmed in the coding of this site.</p>
-        <!-- <p class="description">
-          <a href="https://github.com/k2lh/" target="_blank" aria-label="(opens in new window)">github repository (k2lh)</a>
-        </p>
-        <p class="description">
-          <a href="https://uk.linkedin.com/in/kl-hamilton/" target="_blank" aria-label="(opens in new window)">LinkedIn profile (klhamilton)</a>
-        </p> -->
-      </div>
+      <!-- <div class="boxed">
+        <p class="description">no figmas were harmed in the coding of this site.</p>
+        <p class="description"><a href="https://github.com/k2lh/" target="_blank" aria-label="(opens in new window)">github repository (k2lh)</a></p>
+        <p class="description"><a href="https://uk.linkedin.com/in/kl-hamilton/" target="_blank" aria-label="(opens in new window)">LinkedIn profile (klhamilton)</a></p>
+      </div> -->
     </div>
   </div>
 </template>
@@ -20,15 +16,13 @@
 
 <style scoped>
   .globalFooter {
-    border-top: 1px solid #333;
-    padding: 3rem 0;
+    padding: 5rem 0 0 0;
   }
   .boxed {
     margin: 1rem;
     padding: 1rem;
     font-size: .8rem;
     font-weight: 300;
-    border-top: 1px solid #c6c5c5;
     & p {
       line-height: 1.2;
     }
